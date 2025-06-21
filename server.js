60de6c8d967047b7f6878b50138ec0cfb3c85f22
Shri Karthik A <shrkrthk200518@gmail.com>
2025-06-21 13:59:17 +0530

    Update server.js

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ const app = express()
 // parses json data and makes it available in req.body
 app.use(express.json())
 
-const allowedOrigins = ['http://localhost:5173', 'https://calpal-beige.vercel.app'];
+const allowedOrigins = ['http://localhost:5173', 'https://calpal-app.vercel.app'];
 
 app.use(cors({
   origin: function (origin, callback) {
